11f021676f37a7e397245d4a406f3ce6e0aafe13
WhatWeEat <1053246332@qq.com>
2023-06-01 01:12:57 +0800

    加了异步但没完全加

--- src/main/java/com/cs307/bbsdatabase/BbsDatabaseApplication.java
+++ src/main/java/com/cs307/bbsdatabase/BbsDatabaseApplication.java
@@ -5,10 +5,12 @@ import com.cs307.bbsdatabase.Service.UserService;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableAsync
 @MapperScan("com.cs307.bbsdatabase.Mapper")
 public class BbsDatabaseApplication {
     public static void main(String[] args) {
---
+++ src/main/java/com/cs307/bbsdatabase/Config/AsyncConfig.java
@@ -0,0 +1,41 @@
+package com.cs307.bbsdatabase.Config;
+
+import java.util.concurrent.Executor;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer{
+    @Override
+    @Bean
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        // 核心线程数：线程池创建的时候初始化的线程数
+        executor.setCorePoolSize(10);
+        // 最大线程数：线程池最大的线程数，只有缓冲队列满了之后才会申请超过核心线程数的线程
+        executor.setMaxPoolSize(100);
+        // 缓冲队列：用来缓冲执行任务的队列
+        executor.setQueueCapacity(50);
+        // 线程池关闭：等待所有任务都完成再关闭
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        // 等待时间：等待5秒后强制停止
+        executor.setAwaitTerminationSeconds(5);
+        // 允许空闲时间：超过核心线程之外的线程到达60秒后会被销毁
+        executor.setKeepAliveSeconds(60);
+        // 线程名称前缀
+        executor.setThreadNamePrefix("learn-Async-");
+        // 初始化线程
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return null;
+    }
+}
